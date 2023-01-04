9a2f242be67e34c08bd6e0c7f9f794acfd725b9f
minjaelee <minjaekor@gmail.com>
2023-01-04 13:45:39 +0900

    made newGame function

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,18 @@
 import Cards from './Cards.js';
 import Tikis from './Tikis.js';
 import CurrentChoice from './CurrentChoice.js';
+import {Button} from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import {setNewGame} from '../store.js';
 
 function Game () {
+    let dispatch = useDispatch();
     return (
         <div>
-            <div className="top-layout">
+            <div class="top-layout">
+                <Button className='new-game-btn' onClick={()=>{
+                    dispatch(setNewGame());
+                }}>New Game</Button>
                 <Tikis></Tikis>
                 <CurrentChoice></CurrentChoice>
             </div>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,40 @@ import {tikisState, cardsState} from './data.js';
 let tikis = createSlice({
     name: 'tikis',
     initialState: tikisState,
-
+    reducers: {
+      setNewGame(state) {
+        function shuffle(array) {
+          array.sort(() => Math.random() - 0.5);
+        }
+        const category1 = []
+        const category2 = [];
+        const category3 = [];
+        let newGame = [category1, category2, category3];
+        for (let tiki of state) {
+          if (tiki.category === 1) {
+            category1.push(tiki);
+          } else if (tiki.category === 2) {
+            category2.push(tiki);
+          } else {
+            category3.push(tiki)
+          }
+        }
+        shuffle(category1);
+        shuffle(category2);
+        shuffle(category3);
+        shuffle(newGame)
+        //const temp = [category1, category2, category3]
+        /*for (let i=0; i < 3; i++){
+          let random = Math.floor(Math.random() * temp.length);
+          let popped = temp.splice(random, 1);
+          popped = popped[0];
+          newGame.push(popped)
+        } */
+        newGame = newGame[0].concat(newGame[1], newGame[2]);
+        console.log(newGame)
+        return newGame;
+      }
+    }
 })
 
 let cards = createSlice({
@@ -35,3 +68,4 @@ export default configureStore({
 }) 
 
 export let {setTiki, setCard}  = turnState.actions;
+export let {setNewGame} = tikis.actions;
